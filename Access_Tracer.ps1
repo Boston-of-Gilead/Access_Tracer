56a148f7edbb24c1a5be1099b41112c5c0fca644
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-01 07:53:54 -0400

    Add files via upload

---
+++ Access_Tracer.ps1
@@ -0,0 +1,8 @@
+﻿$filename = read-host -prompt "Paste the UNC of the folder to be queried" #"\\server\shared\FOLDER"
+$user = read-host -prompt "Enter the username to be traced" #"username"
+
+$groups = ([ADSISEARCHER]"samaccountname=$($user)").Findone().Properties.memberof -replace '^CN=([^,]+).+$','$1'
+$acls = (get-acl $fileName).access
+$aclNames = $acls.identityreference.Value
+$aclsForUser = $aclNames | sls $groups | %{$_.tostring()}
+$acls | ? IdentityReference -in $aclsForUser
